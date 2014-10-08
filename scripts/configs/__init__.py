158e209aaa9736a7b7d6a2299d1bf2ed551977be
Matthew Dillon <mrdillon@alaska.edu>
2014-10-07 20:28:18 -0800

    Added basic matsu config.

--- scripts/configs/__init__.py
+++ scripts/configs/__init__.py
@@ -7,12 +7,14 @@ output_path = "../output"
 from .muni import muni_all
 from .chugach import chugach_all
 from .hatcher import hatcher_all
+from .matsu import matsu_all
 from .suppressions import suppression
 
 configList = (
     muni_all, # This config needs to come first
     chugach_all,
     hatcher_all,
+    matsu_all,
     suppression,
     )
 
---
+++ scripts/configs/matsu.py
@@ -0,0 +1,25 @@
+"""Define your configuration below
+"""
+
+import collections
+
+# basic matsu pass config
+
+matsu_all = collections.OrderedDict()
+matsu_all["append"] = None
+matsu_all["tsrs"] = "crs:84"
+matsu_all["source"] = "../source-data/MSB_Trails"
+matsu_all["sql"] = '''
+    SELECT 'Matsu' AS source,
+        name AS system,
+        name_2 AS name,
+        'Matsu/' + name + '/' + name_2 AS identifier,
+        NULL AS surface,
+        NULL AS class,
+        NULL AS lighting,
+        NULL AS difficulty,
+        NULL AS skiType
+    FROM MSB_Trails_Legal_Aug2014
+    '''
+matsu_all["newlayername"] = "matsu_trails"
+
