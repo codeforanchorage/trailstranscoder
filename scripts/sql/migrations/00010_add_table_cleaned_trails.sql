0e2bc09516dd2fd11dad436b63919941eae9dd8d
Matthew Dillon <mrdillon@alaska.edu>
2014-10-14 11:58:37 -0800

    Splitting out migrations scripts.

---
+++ scripts/sql/migrations/00010_add_table_cleaned_trails.sql
@@ -0,0 +1,17 @@
+-- Create table for stashing cleaned trails
+
+CREATE TABLE cleaned_trails (
+    OGC_FID INTEGER PRIMARY KEY ASC,
+    GEOMETRY BLOB,
+    source VARCHAR,
+    system VARCHAR,
+    name VARCHAR,
+    surface VARCHAR,
+    lighting VARCHAR,
+    winter_usage INTEGER,
+    summer_usage INTEGER,
+    ski_difficulty VARCHAR,
+    ski_mode VARCHAR,
+    direction VARCHAR,
+    handicap_accessible VARCHAR);
+
