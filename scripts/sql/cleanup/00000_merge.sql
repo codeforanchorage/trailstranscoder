4136f5de59c562e18052fe9738abdedade3a932b
Matthew Dillon <mrdillon@alaska.edu>
2014-10-14 11:58:46 -0800

    Splitting out merge/cleanup scripts.

---
+++ scripts/sql/cleanup/00000_merge.sql
@@ -0,0 +1,61 @@
+-- Merge trail data from individual tables
+
+INSERT INTO merged_trails (
+    GEOMETRY,
+    source,
+    system,
+    name,
+    identifier,
+    surface,
+    class,
+    lighting,
+    difficulty,
+    skitype)
+SELECT GEOMETRY,
+    source,
+    system,
+    name,
+    identifier,
+    surface,
+    class,
+    lighting,
+    difficulty,
+    skitype
+FROM muni_trails
+UNION
+SELECT GEOMETRY,
+    source,
+    system,
+    name,
+    identifier,
+    surface,
+    class,
+    lighting,
+    difficulty,
+    skitype
+FROM chugach_trails
+UNION
+SELECT GEOMETRY,
+    source,
+    system,
+    name,
+    identifier,
+    surface,
+    class,
+    lighting,
+    difficulty,
+    skitype
+FROM matsu_trails
+UNION
+SELECT GEOMETRY,
+    source,
+    system,
+    name,
+    identifier,
+    surface,
+    class,
+    lighting,
+    difficulty,
+    skitype
+FROM hatcher_pass_trails
+
