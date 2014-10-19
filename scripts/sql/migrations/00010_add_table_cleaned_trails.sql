59c99d3108b1ab9a58f14540f1c11b7178f23dee
Matthew Dillon <mrdillon@alaska.edu>
2014-10-18 16:52:57 -0800

    Making cleaned_trails a layer

--- scripts/sql/migrations/00010_add_table_cleaned_trails.sql
+++ scripts/sql/migrations/00010_add_table_cleaned_trails.sql
@@ -18,3 +18,19 @@ CREATE TABLE cleaned_trails (
     image_url VARCHAR,
     path VARCHAR);
 
+-- In order to take advantage of ogr2ogr, we need to make sure that the DB is aware of this layer.
+INSERT INTO geometry_columns (
+    f_table_name,
+    f_geometry_column,
+    geometry_type,
+    coord_dimension,
+    srid,
+    geometry_format)
+VALUES (
+    'cleaned_trails',
+    'GEOMETRY',
+    5,
+    2,
+    4326,
+    'WKB');
+
